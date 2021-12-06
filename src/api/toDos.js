b7e8c9851f52502338713d1425c87d82113a300e
dailycodemode <steedmanman@gmail.com>
2021-12-06 15:09:22 +0000

    add react to its own folder

--- src/api/toDos.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://europe-west1-irepropev.cloudfunctions.net/api',
-//   headers: {
-//     Authorization: 'Client-ID xIgPSMoJHDQfXblOuC5ATjaFXVMHhjnpQmhDBnjLzEs'
-//   }
-});
