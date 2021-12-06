fb5f2db1d056c849f3d438442230e630be822b48
dailycodemode <35090060+dailycodemode@users.noreply.github.com>
2021-12-06 15:04:25 +0000

    frontEnd

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Auth from "./Auth.js";
+import ToDos from "./ToDos";
+import SearchBar from "./SearchBar.js";
+
+export default function App() {
+ 
+  return (
+    <div id="main">
+      
+<Auth/>
+hi there me
+<br></br>
+<ToDos/>
+<br/>
+<SearchBar/>
+    </div>
+  );
+}
