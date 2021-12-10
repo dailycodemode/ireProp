214d34c359b9474677db35263229aaab047437de
Robert Murphy <robert.murphy@version1.com>
2021-12-10 16:46:04 +0000

    dragonball

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import Auth from "./Auth.js";
-import ToDos from "./ToDos";
-import SearchBar from "./SearchBar.js";
+// import Auth from "./Auth.js";
+// import ToDos from "./ToDos";
+// import SearchBar from "./SearchBar.js";
 
 export default function App() {
  
   return (
     <div id="main">
       
-<Auth/>
+{/* <Auth/> */}
 hi there me
 <br></br>
-<ToDos/>
+{/* <ToDos/>
 <br/>
-<SearchBar/>
+<SearchBar/> */}
     </div>
   );
 }
