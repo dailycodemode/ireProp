58d50fe20a2b2d42f1251ab9355a7f9e72ae0d56
Robert Murphy <robert.murphy@version1.com>
2021-12-13 16:43:24 +0000

    intro update

--- frontend/src/Intro.js
+++ frontend/src/Intro.js
@@ -6,8 +6,8 @@ class Intro extends React.Component {
         return (
             <div>
                 <h3>Intro</h3>
-                <p>Welcome to the property developer</p>
-                <p>The aim of the property developer is to evaluate properties on the website myhome.ie in order to users to make better purchasing decisions</p>
+                <p>Welcome to the Irish property reviewer</p>
+                <p>This app aims to evaluate properties on the website myhome.ie in order to users to make better purchasing decisions</p>
             </div>
         )
     }
